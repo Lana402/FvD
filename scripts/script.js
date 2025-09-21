aa4f2afa23f55c84c0fdf1ed5438e3933133c3dc
Lana402 <lana.tarabar@hva.nl>
2025-09-21 20:29:54 +0200

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("hi");
+
+const button = document.querySelector("nav > button");
+
+button.addEventListener("click", () => {
+  const expanded = button.getAttribute("aria-expanded") === "true";
+  button.setAttribute("aria-expanded", String(!expanded));
+});
+
+const carousel = document.querySelector('.carousel ul');
+
+carousel.addEventListener('scroll', () => {
+    if (carousel.scrollLeft + carousel.clientWidth >= carousel.scrollWidth) {
+        carousel.scrollLeft = 0;
+    }
+});
+
